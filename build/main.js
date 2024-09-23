fc8461884bb02be203af0c9565a38f37a9ba0a8b
Ak-ZeRo-Dev <akzero.dev@gmail.com>
2024-09-23 05:36:29 +0300

    update names

--- build/main.js
+++ build/main.js
@@ -1,16 +1,16 @@
-import { names } from "./names.js";
+import { names } from "./names";
 const table = document.getElementById("names");
 const colNums = 6;
 for (let i = 0; i < names.length; i += colNums) {
-  const row = document.createElement("tr");
-  for (let j = i; j < i + colNums && j < names.length; j++) {
-    const ele = names[j];
-    const col = document.createElement("td");
-    const link = document.createElement("a");
-    link.textContent = ele.name;
-    link.href = ele.description;
-    col.appendChild(link);
-    row.appendChild(col);
-  }
-  table.appendChild(row);
+    const row = document.createElement("tr");
+    for (let j = i; j < i + colNums && j < names.length; j++) {
+        const ele = names[j];
+        const col = document.createElement("td");
+        const link = document.createElement("a");
+        link.textContent = ele.name;
+        link.href = ele.description;
+        col.appendChild(link);
+        row.appendChild(col);
+    }
+    table.appendChild(row);
 }
